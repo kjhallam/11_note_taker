3aeb0ac7fc5f6b39d439bd7482c24ddcf90a2f9b
KHallam <kjhallam321@gmail.com>
2020-11-30 15:24:17 -0600

    revised files routes

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -6,6 +6,10 @@ router.get('/notes', function(req, res) {
 });
 
 router.get('/', function(req, res) {
+    res.json(path.join(__dirname, '/public/index.html'))
+});
+
+router.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 });
 
